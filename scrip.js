e90bc83634253a02bedbd5e19162db5df3f30e28
hoanghuuthuat1357-web <hoanghuuthuat1357@gmail.com>
2026-08-05 13:35:51 +0700

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -6,37 +6,4 @@ function addToCart() {
     document.getElementById("cart-count").textContent = cart;
 
     alert("Đã thêm sản phẩm vào giỏ hàng!");
-const products = [
-    "images/sp1.jpg",
-    "images/sp2.jpg",
-    "images/sp3.jpg"
-];
-
-let current = 0;
-
-function showProduct() {
-    document.getElementById("productImage").src = products[current];
-}
-
-function nextProduct() {
-    current++;
-
-    if (current >= products.length) {
-        current = 0;
-    }
-
-    showProduct();
-}
-
-function prevProduct() {
-    current--;
-
-    if (current < 0) {
-        current = products.length - 1;
-    }
-
-    showProduct();
-}
-
-setInterval(nextProduct, 3000);
 }
